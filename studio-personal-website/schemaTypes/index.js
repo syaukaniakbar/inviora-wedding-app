6392bcbc16a06cc4caa691bc9045a1ab2745132e
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-08-17 23:29:09 +0800

    init sanity schema

---
+++ studio-personal-website/schemaTypes/index.js
@@ -0,0 +1,8 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import project from './project'
+import stack from './stack'
+
+export const schemaTypes = [post, author, category, blockContent, project, stack]
