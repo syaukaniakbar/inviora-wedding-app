239e285d14d1974ca3b82d7a0e9edf809842d29b
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-08-20 00:23:26 +0800

    add project and project detail

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home.jsx";
 import ContactMe from "./pages/ContactMe.jsx";
-import Error404 from "./pages/Error404.jsx"; // Assuming you have an Error404 component
+import Project from "./pages/Project.jsx";
+import ProjectDetail from "./pages/ProjectDetail.jsx";
+import Error404 from "./pages/Error404.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/project/:slug" element={<ProjectDetail />} />
         <Route path="/contact" element={<ContactMe />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
