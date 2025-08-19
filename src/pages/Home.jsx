f38976622942a75ff6056f11d3ca424aeae269e3
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-08-20 00:25:42 +0800

    add marque component be modular

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/custom/Navbar";
 import Hero from "@/components/custom/Hero";
+import MarqueeShowcase from "@/components/custom/MarqueeShowcase";
 import Footer from "@/components/custom/Footer";
 
 function Home() {
   return (
-    <section className="min-h-screen max-h-screen bg-black flex flex-col justify-between box-border">
+    <section className="flex flex-col min-h-screen bg-black justify-between space-y-7 sm:space-y-0">
       <Navbar />
       <Hero />
+      <MarqueeShowcase />
       <Footer />
     </section>
   );
