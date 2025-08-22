c70f89a4a54e67e4cf63feda826cb818a8b0a192
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-08-22 21:45:59 +0800

    using layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,13 @@
-import Navbar from "@/components/custom/Navbar";
+import Layout from "@/layouts/Layout";
 import Hero from "@/components/custom/Hero";
 import MarqueeShowcase from "@/components/custom/MarqueeShowcase";
-import Footer from "@/components/custom/Footer";
 
 function Home() {
   return (
-    <section className="flex flex-col min-h-screen bg-black justify-between space-y-7 sm:space-y-0">
-      <Navbar />
+    <Layout>
       <Hero />
       <MarqueeShowcase />
-      <Footer />
-    </section>
+    </Layout>
   );
 }
 
